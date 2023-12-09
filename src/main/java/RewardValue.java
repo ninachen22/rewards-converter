735b66f3ff6a6f83516b37ad52ca725a99d9c106
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2023-12-09 00:05:24 -0500

    created new rewardValue class, set up constructors and methods

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,13 @@
+public class RewardValue {
+    public RewardValue(double cashValue) {
+    }
+
+    public RewardValue(int milesValue) {
+    }
+
+    public double getCashValue() {
+    }
+
+    public int getMilesValue() {
+    }
+}
