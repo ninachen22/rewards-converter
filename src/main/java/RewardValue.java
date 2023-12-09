56bd0f1931b4f22cf54c6c1c503ddca07ac1ce66
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2023-12-09 00:29:28 -0500

    editted rewardValue methods

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,13 +1,19 @@
 public class RewardValue {
+    int miles;
+    double cash;
     public RewardValue(double cashValue) {
+        this.cash = cashValue;
     }
 
     public RewardValue(int milesValue) {
+        this.miles = milesValue;
     }
 
     public double getCashValue() {
+        return cash;
     }
 
     public int getMilesValue() {
+        return miles;
     }
 }
